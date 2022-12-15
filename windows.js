b423c3cf7c2fda3fd1374b219e8d130cc8651aeb
saluvalasaichandu <101009119+saluvalasaichandu@users.noreply.github.com>
2022-12-15 20:35:12 +0530

    Create windows.js

---
+++ windows.js
@@ -0,0 +1,14 @@
+
+//single element
+//console.log(document.getElementById('my-form'));
+//console.log(document.querySelector('.container'));
+//multiple elements
+//let item=(document.querySelectorAll('.items'));
+//item.forEach((i) => console.log(i));
+let ul=(document.querySelectorAll('.items'));
+ul.firstElementChild.textcontent="HELLO";
+//ul.children[1].innerText="hi";
+//ul.children[1].style.color='yellow'
+
+
+
